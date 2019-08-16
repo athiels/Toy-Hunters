bc2e71d7612779ff92d8fb2ea6b78e24d9226e8c
Xavier Greffe <xgr@bringme.com>
2019-08-16 08:59:16 +0200

    navigate back to trap from hal

--- public/components/hal/hal.js
+++ public/components/hal/hal.js
@@ -1,6 +1,9 @@
 const Hal = {
     template: "#hal-template",
     methods: {
+        clickTrap: function() {
+            this.$router.push({ name: 'Trap' });
+        }
     },
     mounted: function() {
         imageMapResize();
